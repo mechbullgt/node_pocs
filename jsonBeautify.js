d78faca538ff49718961afbaec0156a7cf2a415c
Maqbool Ansari <maqbool.ansari@mindstix.com>
2018-11-11 00:27:57 +0530

    Added demo for package json-beautify

--- jsonBeautify.js
+++ jsonBeautify.js
@@ -1,2 +1,5 @@
 const jsonBeautify = require('json-beautify');
-console.log('Hi');
+var obj = { str: "Hello World", num: 42, smallarray: [ 1, 2, 3, "foo", {} ], smallobject: { foo: "bar", bar: 42 }, bigarray: [ 1, 2, 3, "foo", { foo: "bar", bar: 42, arr: [ 1, 2, 3, "foo", {} ] } ], bigobject: { foo: [ 1, 2, 3, "foo", {} ], bar: 42, a: {b: { c: 42 }}, foobar: "FooBar" } };
+
+console.log(obj);
+console.log(jsonBeautify(obj,null,2,100));
