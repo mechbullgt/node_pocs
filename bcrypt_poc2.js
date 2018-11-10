6e53547b2c285d1e91ca8cb77b3766316fb2cddc
Maqbool Ansari <maqbool.ansari@mindstix.com>
2018-11-11 00:23:01 +0530

    Unable to save hash in local variable.

--- bcrypt_poc2.js
+++ bcrypt_poc2.js
@@ -1,41 +1,56 @@
-const bcrypt= require('bcrypt');
+const bcrypt = require('bcrypt');
 
-const saltRounds =10;
-const userName='mechbullgt';
-const userPassword='aT0Psecret';
-// var encryptedPassword='';
+const saltRounds = 10;
+const userName = 'mechbullgt';
+const userPassword = 'aT0Psecret';
+const wrongUserPassword = 'aTOPsecret';
+var encryptedPassword=[];
 
 console.log('Welcome to bcrypt tutorial');
-checkUser(userName,userPassword);
 
-async function checkUser(userName, userPassword){
+// Calling a async function
+// checkUser(userName, userPassword).then(result=>{
+//     console.log("Execution Sucess");
+// }).catch(error=>{
+//     console.log(error);
+// });
+
+// Calling a async function
+checkUser(userName, wrongUserPassword).then(result=>{
+    console.log("Execution Sucess");
+}).catch(error=>{
+    console.log(error);
+});
+
+async function checkUser(userName, userPassword) {
     console.log('Checking user in our DB!');
-    console.log('User:'+userName+'|'+'Password:'+userPassword);
-    var passwordFromDb= await storeUserPassword(userPassword).then(result=>result.data);
-    console.log('DBPassword:'+passwordFromDb);
-    await bcrypt.compare(userPassword,passwordFromDb, function(err,loginStatus){
-        if(loginStatus){
-            console.log('Welcome Aboard:'+userName);
-            console.log('Userpassword:'+'Before:'+userPassword+'|'+'Encrypted Password:'+storeUserPassword(userPassword));
-        } else {
-            console.log('Do we know you, please check username or password!');
-        }
+    console.log('User:' + userName + '|' + 'Password:' + userPassword);
+    // var passwordFromDb = storeUserPassword(userPassword);
+    // console.log('DBPassword:' + passwordFromDb);
+    bcrypt.genSalt(saltRounds, function (err, salt) {
+        bcrypt.hash(userPassword, salt, function (err, hashedPassword) {
+                //this.encryptedPassword=hashedPassword;
+                console.log('H:'+hashedPassword);
+                encryptedPassword.push(hashedPassword.toString);
+        })
+    })
+    console.log("E:"+encryptedPassword.pop());
+    await bcrypt.compare("aaa", encryptedPassword, function(err,result){
+        console.log('Welcome Aboard:' + userName);
+        console.log('Userpassword:' + 'Before:' + userPassword + '|' + 'Encrypted Password:' + encryptedPassword);
     });
 }
 
-function storeUserPassword(userPassword){
-    return new Promise ((resolve, reject)=>{
-        console.log('Storing User Password!');
-        bcrypt.genSalt(saltRounds,function(err, salt){
-            bcrypt.hash(userPassword, salt, function(err, hashedPassword){
-                if(err){
-                    console.log(err); 
-                } else {
-                    console.log('Normal Password:'+userPassword);
-                    console.log('Hashed Password:'+hashedPassword);    
-                    resolve(hashedPassword);
-                }
-           })
-        })
-})
-}
+
+
+// function storeUserPassword(userPassword) {
+//     console.log('Storing User Password!');
+//     bcrypt.genSalt(saltRounds, function (err, salt) {
+//         bcrypt.hash(userPassword, salt, function (err, hashedPassword) {
+//                 console.log('Normal Password:' + userPassword);
+//                 console.log('Hashed Password:' + hashedPassword);
+//                 encryptedPassword=hashedPassword;
+//         })
+//     })
+//     return encryptedPassword;
+// }
